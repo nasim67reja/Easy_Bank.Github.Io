28df07f658475bb1f3aca8270c4ed7bd0025912e
nasim67reja <nasimreja67@gmail.com>
2022-03-25 05:07:33 -0700

    First Commit 🚀

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 "use strict";
 
+// header
 const header = document.querySelector(".container");
+// modal
+const logInBtn = document.querySelectorAll(".btn");
+const modalWindow = document.querySelector(".modal-window");
+const modalCrossBtn = document.querySelector(".modal-icon");
+// hero
 const hero = document.querySelector(".hero");
 
+// 👉👉👉👉👉 calculate Hero section left margin
 function reportWindowSize() {
   hero.style.marginLeft = getComputedStyle(header)["margin-left"];
 }
@@ -10,9 +17,6 @@ reportWindowSize();
 window.addEventListener("resize", reportWindowSize);
 
 // ///// 👉👉👉👉👉 Open modal functionality
-const logInBtn = document.querySelectorAll(".btn");
-const modalWindow = document.querySelector(".modal-window");
-const modalIconBtn = document.querySelector(".modal-icon");
 
 logInBtn.forEach((el) => {
   el.addEventListener("click", function (e) {
@@ -21,7 +25,8 @@ logInBtn.forEach((el) => {
     document.querySelector("header").style.filter = `blur(3px)`;
   });
 });
-modalIconBtn.addEventListener("click", function (e) {
+
+modalCrossBtn.addEventListener("click", function (e) {
   modalWindow.classList.remove("open-modal");
   document.querySelector("main").style.filter = `blur(0px)`;
   document.querySelector("header").style.filter = `blur(0px)`;
